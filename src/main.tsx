30b22820053a7f49d9239974d3ce52bad748c1c3
AryantoSudarto <aryantosudarto888@gmail.com>
2025-05-12 10:23:26 +0700

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-<<<<<<< HEAD
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-=======
-import { BrowserRouter, Routes, Route } from "react-router";
->>>>>>> edae90bef4601f5a3b9fe6c6699bb107f5c37252
 import "./index.css";
 import App from "./App.tsx";
 
@@ -13,10 +9,7 @@ import MainLayout from "./pages/layout.tsx";
 import AuthLayout from "./pages/auth/layout.tsx";
 
 // Page Import
-<<<<<<< HEAD
 import LandingPage from "./pages/Landing.tsx";
-=======
->>>>>>> edae90bef4601f5a3b9fe6c6699bb107f5c37252
 import HomePage from "./pages/Home.tsx";
 import LeaderboardPage from "./pages/Leaderboard.tsx";
 import ProfilePage from "./pages/Profile.tsx";
@@ -29,34 +22,23 @@ createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <BrowserRouter>
       <Routes>
-<<<<<<< HEAD
         {/* Tanpa layout, standalone */}
         <Route path="/" element={<App />} />
         <Route path="/landing" element={<LandingPage />} />
 
         {/* Dengan layout utama */}
-=======
-        <Route path="/" element={<App />} />
->>>>>>> edae90bef4601f5a3b9fe6c6699bb107f5c37252
         <Route element={<MainLayout />}>
           <Route path="/home" element={<HomePage />} />
           <Route path="/leaderboard" element={<LeaderboardPage />} />
           <Route path="/profile" element={<ProfilePage />} />
         </Route>
-<<<<<<< HEAD
 
         {/* Dengan layout auth */}
-=======
->>>>>>> edae90bef4601f5a3b9fe6c6699bb107f5c37252
         <Route element={<AuthLayout />}>
           <Route path="/register" element={<Register />} />
           <Route path="/login" element={<Login />} />
         </Route>
       </Routes>
     </BrowserRouter>
-<<<<<<< HEAD
   </StrictMode>
-=======
-  </StrictMode>,
->>>>>>> edae90bef4601f5a3b9fe6c6699bb107f5c37252
 );
