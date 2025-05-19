aaa8e4c5b05286e41b726d9ac02a3eedc33d3156
MystiaFin <cytrsx01@gmail.com>
2025-05-19 22:01:27 +0700

    initiate structure for section fetching

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,40 @@
-import "./App.css";
-import MainLogo from "@src/assets/logo.svg";
+import { Link } from "react-router-dom";
+import Logo from "@src/assets/logo.svg";
+import ellipseBg from "@src/assets/ellipse-bg.svg";
 
-const buttonStyle: string = "bg-confirm-blue text-white p-2 rounded-full";
-
-function App() {
+const LandingPage: React.FC = () => {
   return (
-    <main>
-      <h2>Welcome to</h2>
-      <h1>
-        EduWMe <br /> Project
-      </h1>
-      <img src={MainLogo} alt="EduWMe Logo" />
-      <button className={buttonStyle}>Get Started</button>
-    </main>
-  );
-}
+    <div className="relative flex flex-col items-start md:items-center justify-center h-screen overflow-hidden bg-white">
+      {/* Ellipse Background Top Right */}
+      <img
+        src={ellipseBg}
+        alt="ellipse"
+        className="absolute top-0 right-0 w-[50vw] md:w-[30vw] opacity-80"
+      />
 
-export default App;
+      {/* Ellipse Background Bottom Left */}
+      <img
+        src={ellipseBg}
+        alt="ellipse"
+        className="absolute bottom-0 left-0 w-[50vw] md:w-[30vw] rotate-180 opacity-80"
+      />
+
+      <div className="z-10 flex flex-col px-6 md:px-0 md:items-center text-left md:text-center">
+        <h3 className="text-3xl md:text-xl text-[#888D27] font-normal">
+          Welcome to
+        </h3>
+        <h2 className="text-[7vw] md:text-4xl font-bold text-[#888D27] leading-tight">
+          EduWMe <br className="block md:hidden" /> Project
+        </h2>
+        <img src={Logo} alt="GoGo Math" className="w-32 mt-10 md:w-40" />
+        <Link to="/home">
+          <button className="px-6 py-2 mt-10 font-semibold text-white rounded-full shadow-md bg-[#A6B91A]">
+            Get Started
+          </button>
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default LandingPage;
