13472649de54a3bc79f9c21b6ff212d97ad7de06
MystiaFin <cytrsx01@gmail.com>
2025-05-24 08:08:27 +0700

    home styling finished

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,8 +1,35 @@
+import { NavLink } from "react-router";
+import AdditionIcon from "@src/assets/additionIcon.svg";
+import SubtractionIcon from "@src/assets/subtractionIcon.svg";
+import MultiplicationIcon from "@src/assets/multiplicationIcon.svg";
+import DivisionIcon from "@src/assets/divisionIcon.svg";
+
+const ButtonStyle: string =
+  "w-34 h-34 p-2 bg-[#CFB6FF] flex flex-col justify-center items-center rounded-2xl border-4 border-[#374DB0] text-white font-bold text-lg gap-1";
+
 const Home = () => {
   return (
-    <div>
-      <h1>Welcome to the Home Page</h1>
-      <p>This is a simple home page component.</p>
+    <div className="gap-5 flex flex-col mt-20">
+      <section className="flex items-center justify-center gap-5">
+        <NavLink className={ButtonStyle}>
+          <img src={AdditionIcon} />
+          <p>Addition</p>
+        </NavLink>
+        <NavLink className={ButtonStyle}>
+          <img src={SubtractionIcon} />
+          <p>Subtraction</p>
+        </NavLink>
+      </section>
+      <section className="flex items-center justify-center gap-5">
+        <NavLink className={ButtonStyle}>
+          <img src={MultiplicationIcon} />
+          <p className="text-md">Multiplication</p>
+        </NavLink>
+        <NavLink className={ButtonStyle}>
+          <img src={DivisionIcon} />
+          <p>Division</p>
+        </NavLink>
+      </section>
     </div>
   );
 };
