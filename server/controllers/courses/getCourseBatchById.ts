64b92a93abab12d004a9c0d62e19806f9bb913c5
MystiaFin <cytrsx01@gmail.com>
2025-05-31 21:30:20 +0700

    fix import for get coursebatch

--- server/controllers/courses/getCourseBatchById.ts
+++ server/controllers/courses/getCourseBatchById.ts
@@ -1,5 +1,5 @@
 import { Request, Response } from "express";
-import { CourseBatch } from "../../models/CourseBatch.ts";
+import CourseBatch from "../../models/CourseBatch.ts";
 import { courseBatchSchema } from "../../validators/course.validators.ts";
 
 export const getCourseBatchById = async (
