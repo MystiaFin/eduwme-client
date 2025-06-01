ca106fcd926dd24a101a4152eeefcb4602794cec
MystiaFin <cytrsx01@gmail.com>
2025-06-01 17:24:44 +0700

    styling update finished

--- src/pages/auth/layout.tsx
+++ src/pages/auth/layout.tsx
@@ -1,38 +1,32 @@
-import React, { useState } from "react";
-import { useNavigate, useLocation } from "react-router";
-import Register from "./register.tsx";
-import Login from "./login.tsx";
+import { Outlet, NavLink, useLocation } from "react-router";
+import Logo from "@src/assets/logo.svg";
+
+const headerStyle: string =
+  "my-4 text-2xl font-bold text-center text-[#444444] drop-shadow-[0_1px_1px_rgba(255,255,255,1)]";
 
 const AuthLayout = () => {
-  const navigate = useNavigate();
   const location = useLocation();
-  const [isLogin, setIsLogin] = useState(location.pathname === "/login");
-
-  const toggleForm = () => {
-    setIsLogin(!isLogin);
-    navigate(isLogin ? "/register" : "/login");
-  };
+  const isSignIn = location.pathname === "/login";
+  const isSignUp = location.pathname === "/register";
 
   return (
-    <div className="min-h-screen flex items-center justify-center bg-gray-50 py-12 px-4 sm:px-6 lg:px-8">
-      <div className="max-w-md w-full space-y-8">
-        <div>
-          <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
-            {isLogin ? "Sign in to your account" : "Create your account"}
-          </h2>
-          <p className="mt-2 text-center text-sm text-gray-600">
-            {isLogin ? "Don't have an account?" : "Already have an account?"}{" "}
-            <button
-              onClick={toggleForm}
-              className="font-medium text-indigo-600 hover:text-indigo-500"
-            >
-              {isLogin ? "Sign up" : "Sign in"}
-            </button>
-          </p>
-        </div>
-
-        {isLogin ? <Login /> : <Register />}
+    <div>
+      <img src={Logo} alt="EduWMe Logo" className="w-40 mx-auto mt-24" />
+      <div className="flex justify-center">
+        <h1
+          className={headerStyle}
+          style={{ display: isSignIn ? "none" : "flex" }}
+        >
+          Join Us Now!
+        </h1>
+        <h1
+          className={headerStyle}
+          style={{ display: isSignUp ? "none" : "flex" }}
+        >
+          Welcome Back!
+        </h1>
       </div>
+      <Outlet />
     </div>
   );
 };
