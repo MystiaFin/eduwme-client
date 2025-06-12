34acf78ab40364f99e2780e74ecd4910fbf482b6
axel <belaxist@gmail.com>
2025-06-12 21:32:46 +0700

    dist and package update

--- server/routes/shopItemRoutes.ts
+++ server/routes/shopItemRoutes.ts
@@ -1,10 +1,10 @@
 import { Router } from "express";
-import { purchaseItem } from "../controllers/shopItems/purchaseItem.ts";
-import { getUserInventory } from "../controllers/shopItems/getUserInventory.ts";
-import { getShopItems } from "../controllers/shopItems/getShopItems.ts";
-import { equipItem } from "../controllers/shopItems/equipItem.ts";
-import { isAdmin, isUser } from "../middlewares/middleware.ts";
-import { createShopItem } from "../controllers/shopItems/createShopItem.ts";
+import { purchaseItem } from "../controllers/shopItems/purchaseItem.js";
+import { getUserInventory } from "../controllers/shopItems/getUserInventory.js";
+import { getShopItems } from "../controllers/shopItems/getShopItems.js";
+import { equipItem } from "../controllers/shopItems/equipItem.js";
+import { isAdmin, isUser } from "../middlewares/middleware.js";
+import { createShopItem } from "../controllers/shopItems/createShopItem.js";
 
 const router = Router();
 
